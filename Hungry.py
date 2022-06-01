44deefdccded60ab4b1484662f20e77f5f2ae2d2
Ashish-Chauhan7 <ashishchauhan6863@gmail.com>
2022-06-01 18:09:30 +0530

    added water

--- Hungry.py
+++ Hungry.py
@@ -2,4 +2,7 @@ a=input("Are  you hungry:")
 if a.upper()=="YES":
     print("have food")
 else:
-    print("ok drink water")
+    b=input("Are  you Thirsty:")
+if a.upper()=="YES":
+    print("have water")
+    
