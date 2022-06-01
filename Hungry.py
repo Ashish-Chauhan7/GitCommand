be53f3ec3ba2055422a07680362ef07338e78da6
Ashish-Chauhan7 <ashishchauhan6863@gmail.com>
2022-06-01 18:21:49 +0530

    added version1

--- Hungry.py
+++ Hungry.py
@@ -1,8 +1,2 @@
-a=input("Are  you hungry:")
-if a.upper()=="YES":
-    print("have food")
-else:
-    b=input("Are  you Thirsty:")
-if a.upper()=="YES":
-    print("have water")
+print("Version 1.0")
     
